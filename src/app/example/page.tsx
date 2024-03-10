f7da5590ef3b96b866bf0d7ffd6177926d6e7570
prth1234 <107931755+prth1234@users.noreply.github.com>
2024-03-10 12:55:33 +0530

    new

--- src/app/example/page.tsx
+++ src/app/example/page.tsx
@@ -1,61 +1,51 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import { HeroParallaxDemo } from './HeroParallaxDemo';
-import { ParallaxScrollDemo } from './ParallaxScrollDemo';
-import { SparklesPreview } from './SparklesPreview';
-import { TextRevealCardPreview } from './TextRevealCardPreview';
-import { WavyBackgroundDemo } from './WavyBackgroundDemo';
-import { GoogleGeminiEffectDemo } from './gemini-effect';
-import { BackgroundGradientAnimationDemo } from './gradientX';
-import { TabsDemo } from './tabs-preview';
+import { useEffect, useState } from "react";
+import { HeroParallaxDemo } from "./HeroParallaxDemo";
+import { ParallaxScrollDemo } from "./ParallaxScrollDemo";
+import { SparklesPreview } from "./SparklesPreview";
+import { TextRevealCardPreview } from "./TextRevealCardPreview";
+import { WavyBackgroundDemo } from "./WavyBackgroundDemo";
+import { GoogleGeminiEffectDemo } from "./gemini-effect";
+import { BackgroundGradientAnimationDemo } from "./gradientX";
+import { TabsDemo } from "./tabs-preview";
 // import { SparklesPreview2 } from './SparklesPreview2';
-import{LampDemo} from './lamps-view'
-import {FloatingNavDemo} from './navbar'
-import {GridBackgroundDemo} from './gradientDemo'
-import {AnimatedPinDemo} from './pin'
-import {MeteorsDemo} from './BackgroundGradientDemo'
-import {TypewriterEffectSmoothDemo} from './TypewriterEffectSmoothDemo'
-import {SignupFormDemo} from './SignupFormDemo'
-import {MyComponent} from './MyHTML'
-import {SparklesPreview1} from './sparkle_text'
-import {FormsXXX} from './forms'
+import { LampDemo } from "./lamps-view";
+import { FloatingNavDemo } from "./navbar";
+import { GridBackgroundDemo } from "./gradientDemo";
+import { AnimatedPinDemo } from "./pin";
+import { MeteorsDemo } from "./BackgroundGradientDemo";
+import { TypewriterEffectSmoothDemo } from "./TypewriterEffectSmoothDemo";
+import { SignupFormDemo } from "./SignupFormDemo";
+import { MyComponent } from "./MyHTML";
+import { SparklesPreview1 } from "./sparkle_text";
+import { FormsXXX } from "./forms";
 export default function Page() {
   const [isTouchDevice, setIsTouchDevice] = useState(false);
 
-
   useEffect(() => {
-    setIsTouchDevice('ontouchstart' in window || navigator.maxTouchPoints > 0);
+    setIsTouchDevice("ontouchstart" in window || navigator.maxTouchPoints > 0);
   }, []);
 
   return (
     <>
-    {/* <FloatingNavDemo/> */}
+      <FloatingNavDemo />
       <HeroParallaxDemo />
-    
 
-      <LampDemo/>
-
-
+      <LampDemo />
 
       <BackgroundGradientAnimationDemo />
-    {/* <GridBackgroundDemo/> */}
-     <AnimatedPinDemo/>
-     {/* <TypewriterEffectSmoothDemo/> */}
-     {/* <MeteorsDemo/> */}
-     {/* <SignupFormDemo/> */}
-     <MyComponent/>
-     <SparklesPreview1/>
-     <FormsXXX/>
+      {/* <GridBackgroundDemo/> */}
+      <AnimatedPinDemo />
+      {/* <TypewriterEffectSmoothDemo/> */}
+      {/* <MeteorsDemo/> */}
+      {/* <SignupFormDemo/> */}
+      <MyComponent />
+      <SparklesPreview1 />
+      <FormsXXX />
 
       <SparklesPreview />
       <TabsDemo />
-
-    
-
-
-
-      
     </>
   );
 }
--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -1,7 +1,11 @@
-// FloatingNav.jsx
-
+"use client";
 import React, { useState } from "react";
-import { motion, AnimatePresence, useScroll } from "framer-motion";
+import {
+  motion,
+  AnimatePresence,
+  useScroll,
+  useMotionValueEvent,
+} from "framer-motion";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
 
@@ -18,50 +22,56 @@ export const FloatingNav = ({
 }) => {
   const { scrollY } = useScroll();
   const [visible, setVisible] = useState(true);
-  const scrollThreshold = 50; // Adjust the threshold as needed
 
-  const handleScroll = () => {
-    setVisible(scrollY.get() < scrollThreshold);
-  };
+  useMotionValueEvent(scrollY, "change", (current, prev) => {
+    // Check if current and prev are not undefined and are numbers
+    if (typeof current === "number" && typeof prev === "number") {
+      const direction = current - prev;
+
+      if (current < 0) {
+        setVisible(true); // User is scrolling up
+      } else {
+        setVisible(direction < 0); // User is scrolling down
+      }
+    }
+  });
 
   return (
-    <AnimatePresence>
-      {visible && (
-        <motion.div
-          initial={{
-            opacity: 1,
-            y: -100,
-          }}
-          animate={{
-            y: visible ? 0 : -100,
-            opacity: visible ? 1 : 0,
-          }}
-          transition={{
-            duration: 0.2,
-          }}
-          className={cn(
-            "flex max-w-full fixed top-0 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] dark:bg-black bg-black bg-opacity-70 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2 items-center justify-center space-x-4",
-            className
-          )}
-        >
-          {navItems.map((navItem: any, idx: number) => (
-            <Link
-              key={`link=${idx}`}
-              href={navItem.link}
-              className={cn(
-                "relative dark:text-white items-center flex space-x-1 text-white dark:hover:text-white hover:text-gray-300"
-              )}
-            >
-              <span className="block sm:hidden">{navItem.icon}</span>
-              <span className="hidden sm:block text-sm">{navItem.name}</span>
-            </Link>
-          ))}
-          <button className="border text-sm font-medium relative text-white dark:text-black px-4 py-2 rounded-full">
-            <span>Login</span>
-            <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
-          </button>
-        </motion.div>
-      )}
+    <AnimatePresence mode="wait">
+      <motion.div
+        initial={{
+          opacity: 1,
+          y: -100,
+        }}
+        animate={{
+          y: visible ? 0 : -100,
+          opacity: visible ? 1 : 0,
+        }}
+        transition={{
+          duration: 0.2,
+        }}
+        className={cn(
+          "fixed inset-x-0 top-0 z-[5000] mx-auto flex max-w-fit items-center justify-center space-x-4 rounded-full border border-transparent bg-black py-2 pl-8 pr-2 text-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]",
+          className,
+        )}
+      >
+        {navItems.map((navItem: any, idx: number) => (
+          <Link
+            key={`link=${idx}`}
+            href={navItem.link}
+            className={cn(
+              "relative flex items-center space-x-1 text-white hover:text-gray-300",
+            )}
+          >
+            <span className="block sm:hidden">{navItem.icon}</span>
+            <span className="hidden text-sm sm:block">{navItem.name}</span>
+          </Link>
+        ))}
+        <button className="text-red relative rounded-full border border-white px-4 py-2 text-sm font-medium">
+          <span>Login</span>
+          <span className="absolute inset-x-0 -bottom-px mx-auto h-px w-1/2 bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
+        </button>
+      </motion.div>
     </AnimatePresence>
   );
 };
