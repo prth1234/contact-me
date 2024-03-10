ad57ce1ee9c924864633aa6f2509b1beafaad603
prth1234 <107931755+prth1234@users.noreply.github.com>
2024-03-10 16:32:59 +0530

    fre

--- src/app/example/WavyBackgroundDemo.tsx
+++ src/app/example/WavyBackgroundDemo.tsx
@@ -3,13 +3,10 @@ import { WavyBackground } from "@/components/ui/wavy-background";
 
 export function WavyBackgroundDemo() {
   return (
-    <WavyBackground className="max-w-4xl mx-auto" containerClassName="relative">
-      <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
-      Let's Build great tech together 🤝 
-    </p>
-      {/* <p className="text-base md:text-lg px-4 mt-4 text-white font-normal inter-var text-center">
-        How I Can Improve 
-      </p> */}
+    <WavyBackground className="mx-auto max-w-4xl" containerClassName="relative">
+      <p className="inter-var text-center text-2xl font-bold text-white md:text-4xl lg:text-7xl">
+        Let's Build great tech together 🤝
+      </p>
     </WavyBackground>
   );
 }
